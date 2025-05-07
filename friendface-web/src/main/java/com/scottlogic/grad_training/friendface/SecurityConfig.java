1eb18c19ece4d31b0371d840cee4d9cad12feddc
ealbert-scottlogic <ealbert@scottlogic.com>
2025-05-07 15:04:15 +0100

    Added explicit config for cors and changed role assignment on my auth filter to give a user role

--- friendface-web/src/main/java/com/scottlogic/grad_training/friendface/SecurityConfig.java
+++ friendface-web/src/main/java/com/scottlogic/grad_training/friendface/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,6 +28,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
     return http
+            .cors(Customizer.withDefaults())
             .csrf(csrf-> csrf.disable())
             .authorizeHttpRequests(auth ->
             auth.requestMatchers("/userManagement/createUser",
--- friendface-web/src/main/java/com/scottlogic/grad_training/friendface/Sessions/TokenAuthFilter.java
+++ friendface-web/src/main/java/com/scottlogic/grad_training/friendface/Sessions/TokenAuthFilter.java
@@ -7,12 +7,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 public class TokenAuthFilter extends OncePerRequestFilter {
   private final SessionService sessionService;
@@ -25,16 +27,14 @@ public class TokenAuthFilter extends OncePerRequestFilter {
                                   HttpServletResponse response,
                                   FilterChain filterChain)
           throws ServletException, IOException {
-    System.out.println(request.getAuthType());
     System.out.println(request.getHeader("Authorization"));
     String header = request.getHeader("Authorization");
-
     if (header != null && header.startsWith("Bearer ")) {
       String token = header.substring(7);
       User user = sessionService.validateSession(token);
       if (user  != null) {
         UsernamePasswordAuthenticationToken auth =
-          new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+          new UsernamePasswordAuthenticationToken(user, null, List.of(new SimpleGrantedAuthority("USER")));
         SecurityContextHolder.getContext().setAuthentication(auth);
       }
     }
